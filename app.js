70d9fa4915a8f076ce57f84147e6db82a8bceaec
tang <447451242@qq.com>
2017-06-11 21:26:11 +0800

    剃光头

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//设置跨域访问  
+app.all('*', function(req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+    res.header("X-Powered-By",' 3.2.1')  
+    res.header("Content-Type", "application/json;charset=utf-8");  
+    next();  
+});
+
 utils.refreshToken(config);
 app.set('port', process.argv || 3000);
 // view engine setup
